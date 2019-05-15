21abed730ab166ccdb97ae0de2f81ecdea9164f8
Flo Rager Laptop <florianrager90@gmail.com>
2019-05-15 14:17:43 +0200

    started working on the PointExtractor

--- GCodeViewer/GCodeViewer.RenderWindow.Utils/PointExtractor.cs
+++ GCodeViewer/GCodeViewer.RenderWindow.Utils/PointExtractor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
@@ -12,6 +13,7 @@ namespace GCodeViewer.RenderWindow.Utils
     public class PointExtractor
     {
         private FileStream stream;
+        private StreamReader reader;
 
         public PointExtractor(string gcodeFilePath)
         {
@@ -23,11 +25,34 @@ namespace GCodeViewer.RenderWindow.Utils
 
         public Point3DCollection ExtractPoints()
         {
-            Point3DCollection points = new Point3DCollection();
+            using (var reader = new StreamReader(stream))
+            {
+                Point3D point = new Point3D();
+
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                }
+            }
+        }
+
+        private Point3D ExtractPoint(string line, Point3D lastPoint)
+        {
+            Point3D point = lastPoint;
+
+            if (line[0] == ';')
+                return point;
+
+            var xRegex = new Regex("X(/d+/.*/d*)");
+
+            return point;
+        }
+
+        private double GetValue(char character)
+        {
+            var regex = new Regex(character + "((/d+/.+/d+)|(/d+)/s)");
 
 
-
-            return points;
         }
     }
 }
