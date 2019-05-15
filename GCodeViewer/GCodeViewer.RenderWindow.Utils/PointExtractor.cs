d18777d294b11b88b30725c6f7e442250dfa173b
Flo Rager Laptop <florianrager90@gmail.com>
2019-05-15 08:48:46 +0200

    added a more complicated gcode file

---
+++ GCodeViewer/GCodeViewer.RenderWindow.Utils/PointExtractor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace GCodeViewer.RenderWindow.Utils
+{
+    public class PointExtractor
+    {
+        private FileStream stream;
+
+        public PointExtractor(string gcodeFilePath)
+        {
+            if (File.Exists(gcodeFilePath))
+                throw new FileNotFoundException();
+
+            stream = new FileStream(gcodeFilePath, FileMode.Open);
+        }
+
+        public Point3DCollection ExtractPoints()
+        {
+            Point3DCollection points = new Point3DCollection();
+
+
+
+            return points;
+        }
+    }
+}
