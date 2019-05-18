5ea4124cda16668e31d72883e2e8533636c485dd
Flo Rager Laptop <florianrager90@gmail.com>
2019-05-18 10:29:19 +0200

    started working on the ui

---
+++ GCodeViewer/GCodeViewer/Commands/ExtractPaths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GCodeViewer.Commands
+{
+    public class ExtractPaths : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Kahoot is a good game");
+        }
+    }
+}
