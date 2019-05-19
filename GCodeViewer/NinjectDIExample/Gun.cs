af30a4d6e5b1198b6322e95a22e0b96cf542d6af
darager <darager@users.noreply.github.com>
2019-05-19 10:30:46 +0200

    created an example Ioc project for learning purposes

---
+++ GCodeViewer/NinjectDIExample/Gun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectDIExample
+{
+    public class Gun : IWeapon
+    {
+        public string UseWeapon()
+        {
+            return "The gun is used to ";
+        }
+    }
+}
