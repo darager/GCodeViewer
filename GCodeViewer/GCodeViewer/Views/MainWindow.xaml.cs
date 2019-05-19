cb1f2775d07db5e8b6285ed47e294642e9d0f796
darager <darager@users.noreply.github.com>
2019-05-19 22:02:20 +0200

    building ui and editing algorithm

--- GCodeViewer/GCodeViewer/Views/MainWindow.xaml.cs
+++ GCodeViewer/GCodeViewer/Views/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using GCodeViewer.RenderWindow.Utils;
+using Microsoft.Win32;
+using System;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Media3D;
 
@@ -14,6 +20,32 @@ namespace GCodeViewer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".gcode";
+            openFileDialog.Filter = "gcode files (*.gcode)|*.gcode|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string[] text = File.ReadAllLines(filePath);
+
+                for(int i = 0; i < 10; i++)
+                    textblock.AppendText(text[i] + "\n");
+
+                await Task.Factory.StartNew(() =>
+                {
+                    //textblock.Text = File.ReadAllText(openFileDialog.FileName);
+                });
+            }
+        }
+
+        private void Textblock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Debug.WriteLine(e.Changes.ToString());
         }
     }
 }
