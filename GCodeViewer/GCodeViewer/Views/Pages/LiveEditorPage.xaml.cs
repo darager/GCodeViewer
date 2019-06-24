50169ba2aeaf8f644dabb96112a635ba804c38ab
darager <darager@users.noreply.github.com>
2019-06-24 16:43:24 +0200

    did some work on the pointstructure

--- GCodeViewer/GCodeViewer/Views/Pages/LiveEditorPage.xaml.cs
+++ GCodeViewer/GCodeViewer/Views/Pages/LiveEditorPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GCodeViewer.Abstractions.ViewModels;
+using GCodeViewer.RenderWindow.Utils;
 using System;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
 namespace GCodeViewer.Views
@@ -21,8 +23,14 @@ namespace GCodeViewer.Views
 
         private void PopulateRenderWindow()
         {
-            //ScreenSpaceLine3D line = new
-            //liveRenderWindow.Children.Add();
+            var pointExtractor = new PointExtractor(@"C:\Users\florager\source\repos\darager\GCodeViewer\GCodeViewer\Examples\SinkingBenchy.gcode");
+            Point3DCollection points = pointExtractor.ExtractPoints();
+
+            for(int i = 0; i < 10000; i++)
+            {
+                if(i % 10  == 0)
+                    port.AddPoint(points[i].X, points[i].Y, points[i].Z, Colors.DarkRed, 1);
+            }
         }
     }
 }
--- GCodeViewer/GCodeViewer/aa Zombie code/PointExtractor.cs
+++ GCodeViewer/GCodeViewer/aa Zombie code/PointExtractor.cs
@@ -1,100 +1,100 @@
-﻿//using System;
-//using System.IO;
-//using System.Text.RegularExpressions;
-//using System.Windows.Media.Media3D;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Media.Media3D;
 
-//namespace GCodeViewer.RenderWindow.Utils
-//{
-//    public class PointExtractor
-//    {
-//        private FileStream stream;
-//        private Regex xRegex;
-//        private Regex yRegex;
-//        private Regex zRegex;
-//        private char X = 'X';
-//        private char Y = 'Y';
-//        private char Z = 'Z';
-//        private char CommentChar = ';';
+namespace GCodeViewer.RenderWindow.Utils
+{
+    public class PointExtractor
+    {
+        private FileStream stream;
+        private Regex xRegex;
+        private Regex yRegex;
+        private Regex zRegex;
+        private char X = 'X';
+        private char Y = 'Y';
+        private char Z = 'Z';
+        private char CommentChar = ';';
 
-//        public PointExtractor(string gcodeFilePath)
-//        {
-//            if (!File.Exists(gcodeFilePath))
-//            {
-//                throw new FileNotFoundException();
-//            }
+        public PointExtractor(string gcodeFilePath)
+        {
+            if (!File.Exists(gcodeFilePath))
+            {
+                throw new FileNotFoundException();
+            }
 
-//            stream = new FileStream(gcodeFilePath, FileMode.Open);
+            stream = new FileStream(gcodeFilePath, FileMode.Open);
 
-//            xRegex = GetRegex(X);
-//            yRegex = GetRegex(Y);
-//            zRegex = GetRegex(Z);
-//        }
+            xRegex = GetRegex(X);
+            yRegex = GetRegex(Y);
+            zRegex = GetRegex(Z);
+        }
 
-//        public Point3DCollection ExtractPoints()
-//        {
-//            var points = new Point3DCollection();
+        public Point3DCollection ExtractPoints()
+        {
+            var points = new Point3DCollection();
 
-//            using (var reader = new StreamReader(stream))
-//            {
-//                var point = new Point3D();
+            using (var reader = new StreamReader(stream))
+            {
+                var point = new Point3D();
 
-//                string line;
-//                while ((line = reader.ReadLine()) != null)
-//                {
-//                    point = ExtractPoint(line, point);
-//                    points.Add(point);
-//                }
-//            }
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    point = ExtractPoint(line, point);
+                    points.Add(point);
+                }
+            }
 
-//            return points;
-//        }
+            return points;
+        }
 
-//        private Point3D ExtractPoint(string line, Point3D lastPoint)
-//        {
-//            Point3D point = lastPoint;
+        private Point3D ExtractPoint(string line, Point3D lastPoint)
+        {
+            Point3D point = lastPoint;
 
-//            if (string.IsNullOrEmpty(line))
-//            {
-//                return point;
-//            }
+            if (string.IsNullOrEmpty(line))
+            {
+                return point;
+            }
 
-//            if (line[0] == CommentChar)
-//            {
-//                return point;
-//            }
+            if (line[0] == CommentChar)
+            {
+                return point;
+            }
 
-//            Match xMatch = xRegex.Match(line);
-//            Match yMatch = yRegex.Match(line);
-//            Match zMatch = zRegex.Match(line);
+            Match xMatch = xRegex.Match(line);
+            Match yMatch = yRegex.Match(line);
+            Match zMatch = zRegex.Match(line);
 
-//            if (xMatch.Success)
-//            {
-//                point.X = GetValue(xMatch);
-//            }
+            if (xMatch.Success)
+            {
+                point.X = GetValue(xMatch);
+            }
 
-//            if (yMatch.Success)
-//            {
-//                point.Y = GetValue(yMatch);
-//            }
+            if (yMatch.Success)
+            {
+                point.Y = GetValue(yMatch);
+            }
 
-//            if (zMatch.Success)
-//            {
-//                point.Z = GetValue(zMatch);
-//            }
+            if (zMatch.Success)
+            {
+                point.Z = GetValue(zMatch);
+            }
 
-//            return point;
-//        }
-//        private Regex GetRegex(char character)
-//        {
-//            var regex = new Regex(character + "(\\d+\\.\\d+|\\d+)");
-//            return regex;
-//        }
-//        private double GetValue(Match match)
-//        {
-//            string valueString = match.ToString().Substring(1);
-//            double value = Convert.ToDouble(valueString);
+            return point;
+        }
+        private Regex GetRegex(char character)
+        {
+            var regex = new Regex(character + "(\\d+\\.\\d+|\\d+)");
+            return regex;
+        }
+        private double GetValue(Match match)
+        {
+            string valueString = match.ToString().Substring(1);
+            double value = Convert.ToDouble(valueString);
 
-//            return value;
-//        }
-//    }
-//}
+            return value;
+        }
+    }
+}
