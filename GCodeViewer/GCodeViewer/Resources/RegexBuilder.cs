49f57d9f874dc221448e22e34f47358d63b9c15c
darager <darager@users.noreply.github.com>
2019-07-13 20:39:44 +0200

    added regexBuilder + corresponding Tests

---
+++ GCodeViewer/GCodeViewer/Resources/RegexBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GCodeViewer.WPF.Resources
+{
+    public class RegexBuilder
+    {
+        public Regex GetAxisPattern(char axisChar)
+        {
+            string pattern = "\\s" + axisChar + "(\\d+\\.\\d+|\\d+)\\s";
+            var regex = new Regex(pattern);
+
+            return regex;
+        }
+    }
+}
