1c11912abe731714ebeacaf69b5ff9693bb30b85
Rager Florian <florianrager90@gmail.com>
2020-03-12 21:09:22 +0100

    continues opentk introduction

--- src/FirstTest/MainWindow.xaml.cs
+++ src/FirstTest/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace FirstTest
     {
         public MainWindow() => InitializeComponent();
 
-        private void WindowsFormsHost_Initialized(object sender, EventArgs e)
+        private void WindowsFormsHost_Initialized(object sender, EventArgs args)
         {
             var flags = GraphicsContextFlags.Default;
             var control = new GLControl(new GraphicsMode(32, 24), 2, 0, flags);
@@ -21,7 +21,7 @@ namespace FirstTest
             control.Dock = DockStyle.Fill;
             (sender as WindowsFormsHost).Child = control;
 
-            control.Paint += (x, y) =>
+            control.Paint += (s, e) =>
             {
                 GL.ClearColor(0, 0, 255, 1);
 
@@ -29,10 +29,41 @@ namespace FirstTest
                          ClearBufferMask.DepthBufferBit |
                          ClearBufferMask.StencilBufferBit);
 
+                DrawTriangle();
+
+
                 control.SwapBuffers(); // swaps front and back buffers (impo when scene changes?)
             };
 
             control.Invalidate(); // makes control invalid and causes it to be redrawn
+        }
+        int vertexBufferObject;
+        private void DrawTriangle()
+        {
+            float[] vertices = {
+                    -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+                     0.5f, -0.5f, 0.0f, //Bottom-right vertex
+                     0.0f,  0.5f, 0.0f  //Top vertex
+                };
+
+            vertexBufferObject = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferObject);
+
+            // the usage changes according to how the data changes over time (StaticDraw, DynamicDraw, StreamDraw (changes every frame))
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+        }
+
+        private void WindowsFormsHost_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            GL.Viewport(0, 0, 100, 100);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // the buffers have to be cleaned up manually
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0); // binding a buffer to 0 sets it to null
+            GL.DeleteBuffer(vertexBufferObject);
         }
     }
 }
