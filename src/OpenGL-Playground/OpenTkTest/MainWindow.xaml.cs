04e419ebfb044b12bbe2b520e770c33d965d4bb2
Rager Florian <florianrager90@gmail.com>
2020-03-26 17:47:10 +0100

    cleanup

--- src/OpenGL-Playground/OpenTkTest/MainWindow.xaml.cs
+++ src/OpenGL-Playground/OpenTkTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
 using OpenTK;
@@ -10,11 +11,10 @@ namespace OpenTkTest
     public partial class MainWindow : Window
     {
         private GLControl _control;
-        private int _vertexBufferObject;
+        private int _coordinateSystemVBO;
 
         private Shader _shader;
         private Camera _camera;
-        private Random _random = new Random();
 
         public MainWindow()
         {
@@ -32,7 +32,7 @@ namespace OpenTkTest
             _control.MouseMove += Control_MouseMove;
 
             _shader = new Shader("shader/shader.vert", "shader/shader.frag");
-            _camera = new Camera(_shader, new Vector3(0, 0, 10), Vector3.Zero);
+            _camera = new Camera(_shader, new Vector3(0, 0, 9), Vector3.Zero);
 
             _control.Invalidate(); // makes control invalid and causes it to be redrawn
         }
@@ -40,27 +40,24 @@ namespace OpenTkTest
 
         private void Paint(object sender, PaintEventArgs e)
         {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-
+            GL.Enable(EnableCap.PointSmooth);
+            GL.Enable(EnableCap.ProgramPointSize);
+            GL.MatrixMode(MatrixMode.Projection);
 
             float[] vertices =
             {
-                0.0f, 0.0f, 0.0f,
-                0.1f, 0.0f, 0.0f,
-
-                0.0f, 0.0f, 0.0f,
-                0.0f, 0.1f, 0.0f,
-
-                0.0f, 0.0f, 0.0f,
-                0.0f, 0.0f, 0.1f
+                0.0f, 0.0f, 0.0f,   0.1f, 0.0f, 0.0f, // X
+                0.0f, 0.0f, 0.0f,   0.0f, 0.1f, 0.0f, // Y
+                0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.1f, // Z
             };
 
             int count = vertices.Length / 3;
 
-            _vertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            _coordinateSystemVBO = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _coordinateSystemVBO);
             GL.BufferData(BufferTarget.ArrayBuffer,
                           vertices.Length * sizeof(float),
                           vertices,
@@ -72,21 +69,18 @@ namespace OpenTkTest
 
             _shader.Use();
 
-            GL.Enable(EnableCap.PointSmooth);
-            GL.Enable(EnableCap.ProgramPointSize);
-
-            GL.MatrixMode(MatrixMode.Projection);
-
             GL.DrawArrays(PrimitiveType.Lines, 0, count);
+
 
             _control.SwapBuffers(); // swaps front and back buffers (impo when scene changes?)
             _control.Invalidate();
         }
 
+
         private void Window_Unloaded(object sender, RoutedEventArgs e)
         {
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteBuffer(_coordinateSystemVBO);
 
             _shader.Dispose();
         }
@@ -104,17 +98,19 @@ namespace OpenTkTest
             float dx = (float)(e.X - previousPosition.X);
             float dy = (float)(e.Y - previousPosition.Y);
 
+            float mouseSensitivity = 0.25f;
+
             if ((Control.MouseButtons & MouseButtons.Left) != 0)
             {
-                int sensitivity = 5;
 
                 var curr = _camera.CurrentPosition;
 
                 _camera.Reset();
-                _camera.RotateY(-dx / sensitivity);
-                _camera.RotateX(-dy / sensitivity);
+                _camera.RotateY(-dx * mouseSensitivity);
+                _camera.RotateX(-dy * mouseSensitivity);
 
                 _camera.CurrentPosition *= curr;
+                _camera.Update();
             }
 
             previousPosition = new Point(e.X, e.Y);
