28bd0d9286e0f31f68b9bb5832be0b9cccf9e72b
Rager Florian <florianrager90@gmail.com>
2020-04-01 14:58:23 +0200

    add grid renderable for orientation purposes

--- src/Playground/OpenTkTest/PointCloudViewModel.cs
+++ src/Playground/OpenTkTest/PointCloudViewModel.cs
@@ -36,27 +36,59 @@ namespace OpenTkTest
 
             PointCloudObjects.Add(new Renderable(Color.Red, _coordinateSytemVertices, RenderableType.Lines));
 
-            var content = File.ReadLines(@"C:\Users\florager\source\repos\darager\GCodeViewer\src\Examples\SinkingBenchy.gcode");
-            var extractor = new GCodePointExtractor();
-            var points = extractor.ExtractPoints(content);
+            #region Orientation Grid
+            var gridverts = new List<float>();
 
-            var verts = new List<float>();
-            foreach (var point in points)
+            int lineCount = 10;
+            float xSpacing = 2.0f / lineCount;
+            for (int i = 0; i < lineCount + 1; i++)
             {
-                verts.Add(point.X);
-                verts.Add(point.Y);
-                verts.Add(point.Z);
+                float dx = xSpacing * i;
+                gridverts.Add(-1 + dx);
+                gridverts.Add(0);
+                gridverts.Add(1);
+
+                gridverts.Add(-1 + dx);
+                gridverts.Add(0);
+                gridverts.Add(-1);
             }
+            float ySpacing = 2.0f / lineCount;
+            for (int i = 0; i < lineCount + 1; i++)
+            {
+                float dy = xSpacing * i;
+                gridverts.Add(-1);
+                gridverts.Add(0);
+                gridverts.Add(-1 + dy);
 
-            float max = verts.Max();
-            float min = verts.Min();
+                gridverts.Add(1);
+                gridverts.Add(0);
+                gridverts.Add(-1 + dy);
+            }
+            PointCloudObjects.Add(new Renderable(Color.LightGray, gridverts.ToArray(), RenderableType.Lines));
+            #endregion
 
-            var vertices = verts
-                .Take(9000000)
-                .Select(n => Scale(n, min, max, -1, 1))
-                .Reverse()
-                .ToArray();
-            PointCloudObjects.Add(new Renderable(Color.GreenYellow, vertices, RenderableType.Points));
+
+            //var content = File.ReadLines(@"C:\Users\florager\source\repos\darager\GCodeViewer\src\Examples\SinkingBenchy.gcode");
+            //var extractor = new GCodePointExtractor();
+            //var points = extractor.ExtractPoints(content);
+
+            //var verts = new List<float>();
+            //foreach (var point in points)
+            //{
+            //    verts.Add(point.Y);
+            //    verts.Add(point.X);
+            //    verts.Add(point.Z);
+            //}
+
+            //float max = verts.Max();
+            //float min = verts.Min();
+
+            //var vertices = verts
+            //    .Skip(3000)
+            //    //.Take(9000000)
+            //    //.Select(n => Scale(n, min, max, 0, 1))
+            //    .ToArray();
+            //PointCloudObjects.Add(new Renderable(Color.GreenYellow, vertices, RenderableType.Points));
         }
 
         private float Scale(float value, float min, float max, int minScale, int maxScale)
