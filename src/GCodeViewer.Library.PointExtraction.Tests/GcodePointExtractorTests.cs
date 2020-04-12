e9bcb904d865948018d7be5500de749c6f44c890
Rager Florian <florianrager90@gmail.com>
2020-04-13 00:53:55 +0200

    add test for extruder value

--- src/GCodeViewer.Library.PointExtraction.Tests/GcodePointExtractorTests.cs
+++ src/GCodeViewer.Library.PointExtraction.Tests/GcodePointExtractorTests.cs
@@ -18,6 +18,11 @@ namespace GCodeViewer.Library.Tests
             {
                 new string[] { "X0.0 Y0.0 Z0.0", "X10", "X10.0 Z0.0", "Y10.0" },
                 new List<AxisValues> { new AxisValues(0,0,0,0), new AxisValues(10,0,0,0), new AxisValues(10,10,0,0) }
+            },
+            new object[]
+            {
+                new string[] { "X0.0 E1.0", "X10 E2.0", "X10.0 Z0.0", "Y10.0" },
+                new List<AxisValues> { new AxisValues(0,0,0,1), new AxisValues(10,0,0,2), new AxisValues(10,10,0,2) }
             }
         };
         [TestCaseSource(nameof(_extractPrinterAxisValuesTestCases))]
