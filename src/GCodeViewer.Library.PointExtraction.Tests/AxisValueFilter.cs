b6ad9ec580cc3a70da47d4eedf90bbb6ffcb3d66
Rager Florian <florianrager90@gmail.com>
2020-04-13 01:08:13 +0200

    add AxisValueFilter and test

---
+++ src/GCodeViewer.Library.PointExtraction.Tests/AxisValueFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GCodeViewer.Library.Tests
+{
+    public class AxisValueFilter
+    {
+        public IEnumerable<AxisValues> RemoveNonExtrudingPoints(IEnumerable<AxisValues> values)
+        {
+            var prev = AxisValues.NaN;
+
+            foreach (AxisValues value in values)
+            {
+                if (prev.E != value.E)
+                    yield return value;
+
+                prev = value;
+            }
+        }
+    }
+}
--- src/GCodeViewer.Library.PointExtraction.Tests/FilterTests.cs
+++ src/GCodeViewer.Library.PointExtraction.Tests/FilterTests.cs
@@ -5,32 +5,29 @@ using NUnit.Framework;
 namespace GCodeViewer.Library.Tests
 {
     [TestFixture]
-    public class GcodeAxisValueExtractorTests
+    public class FilterTests
     {
-        private readonly static object[] _extractPrinterAxisValuesTestCases =
+        [Test]
+        public void FilterTest()
         {
-            new object[]
+            var values = new List<AxisValues>()
             {
-                new string[] { "X100.0 Y9.0", "Z10"},
-                new List<AxisValues> { new AxisValues(100,9,0,0), new AxisValues(100,9,10,0) }
-            },
-            new object[]
-            {
-                new string[] { "X0.0 Y0.0 Z0.0", "X10", "X10.0 Z0.0", "Y10.0" },
-                new List<AxisValues> { new AxisValues(0,0,0,0), new AxisValues(10,0,0,0), new AxisValues(10,10,0,0) }
-            },
-            new object[]
-            {
-                new string[] { "X0.0 E1.0", "X10 E2.0", "X10.0 Z0.0", "Y10.0" },
-                new List<AxisValues> { new AxisValues(0,0,0,1), new AxisValues(10,0,0,2), new AxisValues(10,10,0,2) }
-            }
-        };
-        [TestCaseSource(nameof(_extractPrinterAxisValuesTestCases))]
-        public void ExtractPrinterAxisValues_ExtractsCorrectValues(string[] lines, IEnumerable<AxisValues> expectedPoints)
-        {
-            var pointExtractor = new GCodeAxisValueExtractor();
+                new AxisValues(0, 0, 0, 0),
+                new AxisValues(10, 3, 0, 0),
+                new AxisValues(10, 3, 0, 2),
+                new AxisValues(0, 0, 0, 0)
+            };
 
-            var actual = pointExtractor.ExtractPrinterAxisValues(lines);
+            var expectedPoints = new List<AxisValues>()
+            {
+                new AxisValues(0, 0, 0, 0),
+                new AxisValues(10, 3, 0, 2),
+                new AxisValues(0, 0, 0, 0)
+            };
+
+            var filter = new AxisValueFilter();
+
+            var actual = filter.RemoveNonExtrudingPoints(values);
 
             actual.Should().Equal(expectedPoints);
         }
