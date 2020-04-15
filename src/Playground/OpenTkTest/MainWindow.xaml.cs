c71cf959813356eee22947d9860fea2f18f6cda0
Rager Florian <florianrager90@gmail.com>
2020-04-15 17:23:20 +0200

    syntax highlighting  now works

--- src/Playground/OpenTkTest/MainWindow.xaml.cs
+++ src/Playground/OpenTkTest/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.IO;
+using System.Reflection;
 using System.Windows;
 using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 
 namespace OpenTkTest
 {
@@ -20,6 +24,19 @@ namespace OpenTkTest
             doc.Text = File.ReadAllText(path);
 
             TextEditor.Document = doc;
+
+            SetupSyntaxHighlighting();
+        }
+
+        public void SetupSyntaxHighlighting()
+        {
+            string xshd_path = @"C:\Users\florager\source\repos\darager\GCodeViewer\src\Playground\OpenTkTest\GCodeSyntaxHighlighting.xml";
+            using var xshd_stream = File.OpenRead(xshd_path);
+
+            var xshd_reader = new XmlTextReader(xshd_stream);
+            TextEditor.SyntaxHighlighting = HighlightingLoader.Load(xshd_reader, HighlightingManager.Instance);
+            xshd_reader.Close();
+            xshd_stream.Close();
         }
     }
 }
