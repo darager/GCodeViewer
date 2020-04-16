7e76acd437d2de8504606a9eab0c4e41ed720451
Rager Florian <florianrager90@gmail.com>
2020-04-16 17:54:19 +0200

    fix errors of usercontrol in new lib

---
+++ src/GCodeViewer.WPF.Controls.TextEditor/GCodeTextEditor.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+
+namespace GCodeViewer.WPF.Controls.TextEditor
+{
+    public partial class GCodeTextEditor : UserControl
+    {
+        public GCodeTextEditor()
+        {
+            InitializeComponent();
+
+            SetupSyntaxHighlighting();
+            LoadGCodeFile();
+        }
+
+        private void SetupSyntaxHighlighting()
+        {
+            string path = @"C:\Users\florager\source\repos\darager\GCodeViewer\src\Playground\OpenTkTest\GCodeSyntaxHighlighting.xml";
+
+            using var stream = File.OpenRead(path);
+            using var reader = new XmlTextReader(stream);
+
+            TextEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+
+            reader.Close();
+            stream.Close();
+        }
+        private void LoadGCodeFile()
+        {
+            var doc = new TextDocument();
+            doc.TextChanged += (s, e) => TextChanged?.Invoke(this, doc.Text);
+
+            string path = @"C:\Users\florager\source\repos\darager\GCodeViewer\src\Examples\SinkingBenchy.gcode";
+            doc.Text = File.ReadAllText(path);
+
+            TextEditor.Document = doc;
+        }
+
+        public EventHandler<string> TextChanged;
+    }
+}
