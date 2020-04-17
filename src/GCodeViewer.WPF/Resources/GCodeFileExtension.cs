43298ede7a43a6f5f2e04cd6d1a89b1ce19ec28e
Rager Florian <florianrager90@gmail.com>
2020-04-17 20:34:55 +0200

    delete old WPF project

--- src/GCodeViewer.WPF/Resources/GCodeFileExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GCodeViewer.WPF.Resources
-{
-    public static class GCodeFileExtensionFilter
-    {
-        public static string Filter = "gcode files (*.gcode)|*.gcode|text files (*.txt)|*.txt|all files (*.*)|*.*";
-        public static string StandardFileExtension = ".gcode";
-    }
-}
