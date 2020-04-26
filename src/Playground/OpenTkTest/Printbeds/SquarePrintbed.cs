4d15eb7571be07cce4ea9a7c38e9490530ec0b82
Florian Rager <florianrager90@gmail.com>
2020-04-26 23:12:33 +0200

    remove old printbed code

--- src/Playground/OpenTkTest/Printbeds/SquarePrintbed.cs
+++ src/Playground/OpenTkTest/Printbeds/SquarePrintbed.cs
@@ -8,5 +8,45 @@ namespace OpenTkTest.Printbeds
 {
     public class SquarePrintbed
     {
+        //var gridverts = new List<float>();
+
+        //int lineCount = 10;
+        //float xSpacing = 2.0f / lineCount;
+        //for (int i = 0; i < lineCount + 1; i++)
+        //{
+        //    float dx = xSpacing * i;
+        //    gridverts.Add(-1 + dx);
+        //    gridverts.Add(0);
+        //    gridverts.Add(1);
+
+        //    gridverts.Add(-1 + dx);
+        //    gridverts.Add(0);
+        //    gridverts.Add(-1);
+        //}
+        //float ySpacing = 2.0f / lineCount;
+        //for (int i = 0; i < lineCount + 1; i++)
+        //{
+        //    float dy = xSpacing * i;
+        //    gridverts.Add(-1);
+        //    gridverts.Add(0);
+        //    gridverts.Add(-1 + dy);
+
+        //    gridverts.Add(1);
+        //    gridverts.Add(0);
+        //    gridverts.Add(-1 + dy);
+        //}
+        //PointCloudObjects.Add(new Renderable(Color.LightGray, gridverts.ToArray(), RenderableType.Lines));
+
+        //float[] verts =
+        //{
+        //    1, 0, 1,
+        //    1, 0, -1,
+        //    -1, 0, -1,
+
+        //    -1, 0, 1,
+        //    -1, 0, -1,
+        //    1, 0, 1,
+        //};
+        //PointCloudObjects.Add(new Renderable(Color.DarkGray, verts, RenderableType.Triangles));
     }
 }
--- src/Playground/OpenTkTest/ViewModels/Viewer3DViewModel.cs
+++ src/Playground/OpenTkTest/ViewModels/Viewer3DViewModel.cs
@@ -37,49 +37,6 @@ namespace OpenTkTest.ViewModels
             PointCloudObjects = new ObservableCollection<Renderable>();
 
             PointCloudObjects.Add(new Renderable(Color.Red, _coordinateSytemVertices, RenderableType.Lines));
-
-            #region Orientation Grid
-            var gridverts = new List<float>();
-
-            int lineCount = 10;
-            float xSpacing = 2.0f / lineCount;
-            for (int i = 0; i < lineCount + 1; i++)
-            {
-                float dx = xSpacing * i;
-                gridverts.Add(-1 + dx);
-                gridverts.Add(0);
-                gridverts.Add(1);
-
-                gridverts.Add(-1 + dx);
-                gridverts.Add(0);
-                gridverts.Add(-1);
-            }
-            float ySpacing = 2.0f / lineCount;
-            for (int i = 0; i < lineCount + 1; i++)
-            {
-                float dy = xSpacing * i;
-                gridverts.Add(-1);
-                gridverts.Add(0);
-                gridverts.Add(-1 + dy);
-
-                gridverts.Add(1);
-                gridverts.Add(0);
-                gridverts.Add(-1 + dy);
-            }
-            PointCloudObjects.Add(new Renderable(Color.LightGray, gridverts.ToArray(), RenderableType.Lines));
-
-            float[] verts =
-            {
-                1, 0, 1,
-                1, 0, -1,
-                -1, 0, -1,
-
-                -1, 0, 1,
-                -1, 0, -1,
-                1, 0, 1,
-            };
-            PointCloudObjects.Add(new Renderable(Color.DarkGray, verts, RenderableType.Triangles));
-            #endregion
         }
 
         private Renderable _model;
