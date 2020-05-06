5ef7c202bcf2268a1f9a3a3bf9da09227b06eaf3
Florian Rager <florianrager90@gmail.com>
2020-05-06 23:11:04 +0200

    create bindable properties

--- src/GCodeViewer.WPF.Controls/NumericUpDownViewModel.cs
+++ src/GCodeViewer.WPF.Controls/NumericUpDownViewModel.cs
@@ -1,18 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Windows.Media;
 
 namespace GCodeViewer.WPF.Controls
 {
     public class NumericUpDownViewModel : INotifyPropertyChanged
     {
-        float Value;
-        float MinValue;
-        float MaxValue;
-        float StepSize;
+        public float Value
+        {
+            get => _value;
+            set
+            {
+                if (value == _value) return;
+
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+        private float _value;
+
+        public float MinValue
+        {
+            get => _minValue;
+            set
+            {
+                if (value == _minValue) return;
+
+                _minValue = value;
+                OnPropertyChanged("MinValue");
+            }
+        }
+        private float _minValue;
+
+        public float MaxValue
+        {
+            get => _maxValue;
+            set
+            {
+                if (value == _maxValue) return;
+
+                _maxValue = value;
+                OnPropertyChanged("MaxValue");
+            }
+        }
+        private float _maxValue;
+
+        public float StepSize
+        {
+            get => _stepSize;
+            set
+            {
+                if (value == _stepSize) return;
+
+                _stepSize = value;
+                OnPropertyChanged("StepSize");
+            }
+        }
+        private float _stepSize;
+
+        public Brush Foreground
+        {
+            get => _foreground;
+            set
+            {
+                if (value == _foreground) return;
+
+                _foreground = value;
+                OnPropertyChanged("Foreground");
+            }
+        }
+        private Brush _foreground;
+
+        public Brush Background
+        {
+            get => _background;
+            set
+            {
+                if (value == _background) return;
+
+                _background = value;
+                OnPropertyChanged("Background");
+            }
+        }
+        private Brush _background;
+
+        public NumericUpDownViewModel()
+        {
+        }
 
         private void OnPropertyChanged(string propertyName)
         {
