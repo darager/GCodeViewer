6929333f72ede8b9b8654612bd0789e153674495
Florian Rager <florianrager90@gmail.com>
2020-05-08 10:55:43 +0200

    add outside viewmodel for testing

--- src/UserControlTest/MainWindow.xaml.cs
+++ src/UserControlTest/MainWindow.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UserControlTest
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new NumUpDownViewModel();
         }
+    }
+
+    public class NumUpDownViewModel : INotifyPropertyChanged
+    {
+        private float _value;
+
+        public float Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
