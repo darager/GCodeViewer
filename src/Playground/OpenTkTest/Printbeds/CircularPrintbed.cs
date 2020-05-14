461249d5fe28b8b2a1aa74eff9f016e2adb74dcc
Florian Rager <florianrager90@gmail.com>
2020-05-14 19:29:25 +0200

    vertical lines in printbed work the horizontal ones however do not work like they are supposed to

--- src/Playground/OpenTkTest/Printbeds/CircularPrintbed.cs
+++ src/Playground/OpenTkTest/Printbeds/CircularPrintbed.cs
@@ -9,23 +9,52 @@ namespace OpenTkTest.Printbeds
     {
         private List<Renderable> _parts = new List<Renderable>();
 
-        public CircularPrintbed(float radius, Color color, Color lineColo, int triangleCount = 64)
+        public CircularPrintbed(float radius, Color color, Color lineColo, int triangleCount = 100)
         {
             List<float> printbedVerts = GetPrintbedVerts(radius, triangleCount);
             var printbed = new Renderable(color, printbedVerts.ToArray(), RenderableType.Triangles);
 
-            List<float> lineVerts = GetLineVertices(radius, 101);
+            List<float> lineVerts = GetLineVertices(radius);
             var lines = new Renderable(lineColo, lineVerts.ToArray(), RenderableType.Lines);
 
             _parts.Add(printbed);
             _parts.Add(lines);
         }
 
-        // TODO: implement
-        private List<float> GetLineVertices(float radius, int lineCount)
+        // TODO: fix the horizontal lines
+        private List<float> GetLineVertices(float radius, int lineCount = 11)
         {
-            return new List<float>();
+            var verts = new List<float>();
+
+            float lineSpacing = (2 * radius) / lineCount;
+
+            for (int i = 0; i < lineCount - 1; i++)
+            {
+                float x = i * lineSpacing;
+                float y = (float)Math.Sqrt((radius * radius) - (x * x));
+
+                // vertical Lines
+                AddPoint(verts, x, y);
+                AddPoint(verts, x, -y);
+                AddPoint(verts, -x, y);
+                AddPoint(verts, -x, -y);
+
+                // horizontal Lines
+                AddPoint(verts, y, x);
+                AddPoint(verts, y, -x);
+                AddPoint(verts, -y, x);
+                AddPoint(verts, -y, -x);
+            }
+
+            return verts;
         }
+        private void AddPoint(List<float> vertices, float x, float y)
+        {
+            vertices.Add(x);
+            vertices.Add(0);
+            vertices.Add(y);
+        }
+
         private static List<float> GetPrintbedVerts(float radius, int triangleCount)
         {
             var verts = new List<float>();
