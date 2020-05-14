03c11dda59f2a37ca2ba1fee69cb57023a324048
Florian Rager <florianrager90@gmail.com>
2020-05-14 23:35:04 +0200

    fix stl to renderable after some trial and error the stl is properly converted into something which can be displayed by the 3D-View

--- src/Playground/OpenTkTest/MainWindow.xaml.cs
+++ src/Playground/OpenTkTest/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using GCodeViewer.Helpers;
 using g3;
 using GCodeViewer.WPF.Controls.PointCloud;
 using OpenTkTest.ViewModels;
+using System;
 
 namespace OpenTkTest
 {
@@ -32,13 +33,13 @@ namespace OpenTkTest
 
         private void LoadAndDisplayModelFromStl()
         {
-            string file = "Benchy_Christmas_1.stl";
+            string file = @"STL-Files\Benchy_Christmas_1.stl";
 
             using var stream = File.OpenRead(file);
             using var binaryReader = new BinaryReader(stream);
 
-            var reader = new STLReader();
             var builder = new SimpleMeshBuilder();
+            var reader = new STLReader();
 
             var result = reader.Read(binaryReader, ReadOptions.Defaults, builder);
 
@@ -46,18 +47,31 @@ namespace OpenTkTest
             {
                 var mesh = builder.Meshes[0];
 
-                var dVerts = mesh.Vertices;
+                var triangleIndices = mesh.TriangleIndices();
 
-                float max = (float)dVerts.Max();
-                float min = (float)dVerts.Min();
+                float max = (float)mesh.Vertices.back;
+                float min = (float)mesh.Vertices.front;
 
                 var verts = new List<float>();
-                foreach (float vert in dVerts)
+                foreach (int idx in triangleIndices)
                 {
-                    verts.Add(vert.Scale(min, max, -0.5f, 0.5f));
+                    var triangle = mesh.GetTriangle(idx);
+
+                    AddPoint(triangle.a);
+                    AddPoint(triangle.b);
+                    AddPoint(triangle.c);
+
+                    void AddPoint(int vertexIndex)
+                    {
+                        Vector3d vert = mesh.GetVertex(vertexIndex);
+
+                        verts.Add(((float)vert.x).Scale(min, max, -0.5f, 0.5f));
+                        verts.Add(((float)vert.y).Scale(min, max, -0.5f, 0.5f));
+                        verts.Add(((float)vert.z).Scale(min, max, -0.5f, 0.5f));
+                    }
                 }
 
-                var model = new Renderable(Color.Aqua, verts.ToArray(), RenderableType.Points);
+                var model = new Renderable(Color.Aqua, verts.ToArray(), RenderableType.Triangles);
                 _viewer3DVM.PointCloudObjects.Add(model);
             }
 
