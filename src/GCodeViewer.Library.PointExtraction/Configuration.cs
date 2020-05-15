9022d681865b2d5304f0889ab61776a410a96e23
Florian Rager <florianrager90@gmail.com>
2020-05-15 23:33:11 +0200

    use newtonsoft to write and load config file

---
+++ src/GCodeViewer.Library.PointExtraction/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GCodeViewer.Library
+{
+    public class Configuration
+    {
+        public PrintArea PrintArea { get; set; } = new PrintArea();
+
+        public async Task SaveConfiguration(FileStream stream, CancellationToken token)
+        {
+            var writer = new StreamWriter(stream);
+            var text = JsonConvert.SerializeObject(this);
+
+            await writer.WriteAsync(text);
+            writer.Close();
+        }
+        public async static Task<Configuration> LoadConfiguration(FileStream stream)
+        {
+            var reader = new StreamReader(stream);
+            var text = await reader.ReadToEndAsync();
+            reader.Close();
+
+            return JsonConvert.DeserializeObject<Configuration>(text);
+        }
+    }
+
+    public class PrintArea
+    {
+        public float Diameter { get; set; } = 100;
+        public float Height { get; set; } = 100;
+    }
+}
--- src/UserControlTest/MainWindow.xaml.cs
+++ src/UserControlTest/MainWindow.xaml.cs
@@ -1,22 +1,46 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using GCodeViewer.Library;
 
 namespace UserControlTest
 {
     public partial class MainWindow : Window
     {
-        NumUpDownViewModel _vm;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            _vm = new NumUpDownViewModel();
-            // TODO: when this is used the controls looks fine
-            //       but when DataContext of the control itself is set nothing works anymore
-            //       I am completely and utterly lost for words!  :'(
-            _vm.NumValue = 50;
+            TestOutConfigFunctionality().Wait();
+        }
 
-            this.DataContext = _vm;
+        private static async Task TestOutConfigFunctionality()
+        {
+            string fileName = "gcodeviewer-config.json";
+
+            string directoryPath = "Configuration";
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            bool fileExists = File.Exists(filePath);
+
+            if (!fileExists)
+            {
+                using var stream = File.Create(filePath);
+
+                var config = new Configuration();
+                await config.SaveConfiguration(stream, CancellationToken.None);
+                stream.Close();
+            }
+            //else
+            //{
+            //    using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            //    var conf = await Configuration.LoadConfiguration(stream);
+            //    stream.Close();
+            //}
         }
     }
 }
