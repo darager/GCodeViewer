c65a4ed22b03786bfa4099d94a75632118e43a0a
Florian Rager <florianrager90@gmail.com>
2020-05-17 13:41:18 +0200

    handle right pane over viewmodel

--- src/GCodeViewer.WPF/Pages/MainWindow.xaml.cs
+++ src/GCodeViewer.WPF/Pages/MainWindow.xaml.cs
@@ -5,11 +5,12 @@ namespace GCodeViewer.WPF.Pages
 {
     public partial class MainWindow : Window
     {
-        public MainWindow(Viewer3DViewModel viewerVM)
+        public MainWindow(Viewer3DViewModel viewerVM, PagingViewModel pagingVM)
         {
             InitializeComponent();
 
             Viewer3D.DataContext = viewerVM;
+            Frame.DataContext = pagingVM;
         }
     }
 }
---
+++ src/GCodeViewer.WPF/ViewModels/PagingViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+using GCodeViewer.WPF.Pages;
+
+namespace GCodeViewer.WPF.ViewModels
+{
+    public class PagingViewModel : INotifyPropertyChanged
+    {
+        private Page _page;
+
+        public Page Page
+        {
+            get => _page;
+            set
+            {
+                if (_page == value) return;
+                _page = value;
+
+                OnPropertyChanged("Page");
+            }
+        }
+
+        public PagingViewModel()
+        {
+            // HACK
+            Page = new TextEditorPage();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
