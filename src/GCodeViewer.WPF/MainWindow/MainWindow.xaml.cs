71d59f75a3c2977370efac5291677eb6a4718dc1
Florian Rager <florianrager90@gmail.com>
2020-05-20 17:14:56 +0200

    no longer  use names to set the datacontexts of controls

--- src/GCodeViewer.WPF/MainWindow/MainWindow.xaml.cs
+++ src/GCodeViewer.WPF/MainWindow/MainWindow.xaml.cs
@@ -1,22 +1,20 @@
 ﻿using System.Windows;
-using GCodeViewer.WPF.Controls.PointCloud;
 
 namespace GCodeViewer.WPF.MainWindow
 {
     public partial class MainWindow : Window
     {
-        private Viewer3DViewModel _viewerVM;
-        private PagingViewModel _pagingVM;
+        public Viewer3DViewModel Viewer3DViewModel { get; }
+        public PagingViewModel PagingViewModel { get; }
 
         public MainWindow(Viewer3DViewModel viewerVM, PagingViewModel pagingVM)
         {
             InitializeComponent();
 
-            _viewerVM = viewerVM;
-            _pagingVM = pagingVM;
+            Viewer3DViewModel = viewerVM;
+            PagingViewModel = pagingVM;
 
-            Viewer3D.DataContext = _viewerVM;
-            Frame.DataContext = _pagingVM;
+            DataContext = this;
         }
     }
 }
