d56ffc9c8c149bb92b8e8fc4121bdcaca605b592
Florian Rager <florianrager90@gmail.com>
2020-07-08 11:21:26 +0200

    Write to STL File experimentation

--- src/Playground/OpenTkTest/MainWindow.xaml.cs
+++ src/Playground/OpenTkTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows;
@@ -28,14 +29,16 @@ namespace OpenTkTest
 
             Statusbar.DataContext = new StatusBarViewModel(_textEditorVM);
 
-            LoadAndDisplayModelFromStl();
+            string filePath = @"STL-Files\xyz-calibrationCube.stl";
+
+            var mesh = LoadMesh(filePath);
+            DisplayMesh(mesh);
+            SaveMesh(mesh, filePath);
         }
 
-        private void LoadAndDisplayModelFromStl()
+        private SimpleMesh LoadMesh(string filePath)
         {
-            string file = @"STL-Files\xyz-calibrationCube.stl";
-
-            using var stream = File.OpenRead(file);
+            using var stream = File.OpenRead(filePath);
             using var binaryReader = new BinaryReader(stream);
 
             var builder = new SimpleMeshBuilder();
@@ -43,51 +46,71 @@ namespace OpenTkTest
 
             var result = reader.Read(binaryReader, ReadOptions.Defaults, builder);
 
+            binaryReader.Close();
+            stream.Close();
+
             if (result.code == IOCode.Ok)
             {
-                var mesh = builder.Meshes[0];
-
-                var triangleIndices = mesh.TriangleIndices();
-
-                // TODO: in the future these are controled by the printvolume set in the renderpipeline
-                // max and min are not retrieved perfectly
-                float max = (float)mesh.Vertices.back;
-                float min = (float)mesh.Vertices.front;
-
-                var points = new List<Point3D>();
-                foreach (int idx in triangleIndices)
-                {
-                    var triangle = mesh.GetTriangle(idx);
-
-                    // TODO: this is drawing some lines twice many lines
-                    AddPoint(triangle.c);
-                    AddPoint(triangle.a);
-                    AddPoint(triangle.a);
-                    AddPoint(triangle.b);
-                    AddPoint(triangle.b);
-                    AddPoint(triangle.c);
-
-                    void AddPoint(int vertexIndex)
-                    {
-                        Vector3d vert = mesh.GetVertex(vertexIndex);
-
-                        float x = ((float)vert.x).Scale(min, max, -0.5f, 0.5f);
-                        float y = ((float)vert.y).Scale(min, max, -0.5f, 0.5f);
-                        float z = ((float)vert.z).Scale(min, max, -0.5f, 0.5f);
-
-                        points.Add(new Point3D(x, y, z));
-                    }
-                }
-
-                var model = new Renderable(Color.Aqua, points, RenderableType.Lines);
-                _viewer3DVM.Add(model);
+                return builder.Meshes[0];
             }
+            else
+                throw new Exception("Reading the STL file went wrong.");
+        }
 
-            binaryReader.Close();
+        private void SaveMesh(SimpleMesh mesh, string filePath)
+        {
+            using var stream = File.OpenWrite(filePath);
+            var binaryWriter = new BinaryWriter(stream);
+
+            var writemesh = new WriteMesh(mesh);
+
+            var writer = new STLWriter();
+            writer.Write(binaryWriter, new List<WriteMesh>() { writemesh }, WriteOptions.Defaults);
+
+            binaryWriter.Close();
             stream.Close();
         }
 
+        private void DisplayMesh(SimpleMesh mesh)
+        {
+            var triangleIndices = mesh.TriangleIndices();
+
+            // TODO: in the future these are controled by the printvolume set in the renderpipeline
+            // max and min are not retrieved perfectly
+            float max = (float)mesh.Vertices.back;
+            float min = (float)mesh.Vertices.front;
+
+            var points = new List<Point3D>();
+            foreach (int idx in triangleIndices)
+            {
+                var triangle = mesh.GetTriangle(idx);
+
+                // TODO: this is drawing some lines twice many lines
+                AddPoint(triangle.c);
+                AddPoint(triangle.a);
+                AddPoint(triangle.a);
+                AddPoint(triangle.b);
+                AddPoint(triangle.b);
+                AddPoint(triangle.c);
+
+                void AddPoint(int vertexIndex)
+                {
+                    Vector3d vert = mesh.GetVertex(vertexIndex);
+
+                    float x = ((float)vert.x).Scale(min, max, -0.5f, 0.5f);
+                    float y = ((float)vert.y).Scale(min, max, -0.5f, 0.5f);
+                    float z = ((float)vert.z).Scale(min, max, -0.5f, 0.5f);
+
+                    points.Add(new Point3D(x, y, z));
+                }
+            }
+
+            var model = new Renderable(Color.Aqua, points, RenderableType.Lines);
+            _viewer3DVM.Add(model);
+        }
+
         private Renderable movemodel;
+
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             if (movemodel != null)
