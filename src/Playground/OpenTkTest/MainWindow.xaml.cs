25ffc6173dfad40cb7a2575bc9993a2ddac505bf
Florian Rager <florianrager90@gmail.com>
2020-07-08 11:46:35 +0200

    Load and Write all meshes

--- src/Playground/OpenTkTest/MainWindow.xaml.cs
+++ src/Playground/OpenTkTest/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using g3;
 using GCodeViewer.Helpers;
@@ -31,12 +32,40 @@ namespace OpenTkTest
 
             string filePath = @"STL-Files\xyz-calibrationCube.stl";
 
-            var mesh = LoadMesh(filePath);
-            DisplayMesh(mesh);
-            SaveMesh(mesh, filePath);
+            var meshes = LoadMeshes(filePath);
+
+            // cut mesh
+            //var meshbuilder = new DMesh3Builder();
+
+            //var triangles = new List<(int, int, int)>();
+            //for (int i = 0; i < mesh.Triangles.Count() - 3; i += 3)
+            //{
+            //    var t1 = mesh.Triangles[i];
+            //    var t2 = mesh.Triangles[i + 1];
+            //    var t3 = mesh.Triangles[i + 2];
+            //    triangles.Add((t1, t2, t3));
+            //}
+            //triangles.ForEach(t => meshbuilder.AppendTriangle(t.Item1, t.Item2, t.Item3));
+
+            //var vertices = new List<Vector3d>();
+            //for (int i = 0; i < mesh.Vertices.Count() - 3; i += 3)
+            //{
+            //    var vs = mesh.Vertices;
+            //    vertices.Add(new Vector3d(vs[i], vs[i + 1], vs[i + 2]));
+            //}
+            //vertices.ForEach(t => meshbuilder.AppendVertex(new NewVertexInfo(t)));
+            //var dmesh = meshbuilder.Meshes[0];
+
+            //var cut = new MeshPlaneCut(dmesh, new Vector3d(1, 1, 1), new Vector3d(1, 0, 0));
+            //cut.Cut();
+
+            foreach (var mesh in meshes)
+                DisplayMesh(mesh);
+
+            SaveMeshes(meshes, filePath);
         }
 
-        private SimpleMesh LoadMesh(string filePath)
+        private List<SimpleMesh> LoadMeshes(string filePath)
         {
             using var stream = File.OpenRead(filePath);
             using var binaryReader = new BinaryReader(stream);
@@ -49,23 +78,22 @@ namespace OpenTkTest
             binaryReader.Close();
             stream.Close();
 
-            if (result.code == IOCode.Ok)
-            {
-                return builder.Meshes[0];
-            }
-            else
-                throw new Exception("Reading the STL file went wrong.");
+            if (result.code != IOCode.Ok)
+                throw new Exception("Something went wrong when reading the STL file!");
+
+            return builder.Meshes;
         }
 
-        private void SaveMesh(SimpleMesh mesh, string filePath)
+        private void SaveMeshes(List<SimpleMesh> meshes, string filePath)
         {
             using var stream = File.OpenWrite(filePath);
             var binaryWriter = new BinaryWriter(stream);
 
-            var writemesh = new WriteMesh(mesh);
+            var writeMeshes = meshes.Select(m => new WriteMesh(m))
+                                    .ToList();
 
             var writer = new STLWriter();
-            writer.Write(binaryWriter, new List<WriteMesh>() { writemesh }, WriteOptions.Defaults);
+            writer.Write(binaryWriter, writeMeshes, WriteOptions.Defaults);
 
             binaryWriter.Close();
             stream.Close();
