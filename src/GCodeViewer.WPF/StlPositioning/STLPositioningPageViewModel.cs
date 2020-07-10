6768e7b16d9f326cbf133d47fdf25060a1a2bab3
Florian Rager <florianrager90@gmail.com>
2020-07-10 17:08:00 +0200

    Implement Cancel feature

--- src/GCodeViewer.WPF/StlPositioning/STLPositioningPageViewModel.cs
+++ src/GCodeViewer.WPF/StlPositioning/STLPositioningPageViewModel.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Windows.Input;
 using GCodeViewer.Library;
 using GCodeViewer.Library.Renderables;
 using GCodeViewer.Library.Renderables.Things;
+using GCodeViewer.WPF.MVVM.Helpers;
+using GCodeViewer.WPF.Navigation;
 
 namespace GCodeViewer.WPF.StlPositioning
 {
     public class STLPositioningPageViewModel : INotifyPropertyChanged
     {
         private IViewerScene _scene;
+        private ICompositeRenderable _model;
+        private PageNavigationService _pageNavigation;
 
-        public STLPositioningPageViewModel(IViewerScene scene)
+        public ICommand Cancel { get; private set; }
+
+        public STLPositioningPageViewModel(IViewerScene scene, PageNavigationService navService)
         {
             _scene = scene;
+            _pageNavigation = navService;
+
+            this.Cancel = new RelayCommand(RemoveModelFromView);
         }
 
         public void LoadSTL(string filePath)
@@ -21,9 +32,16 @@ namespace GCodeViewer.WPF.StlPositioning
             var file = new STLFile(filePath);
             List<Mesh> meshes = file.LoadMeshes();
 
-            var wireframe = new Wireframe(meshes[0], Color.GreenYellow, Color.DarkGreen);
+            _model = new Wireframe(meshes[0], Color.GreenYellow, Color.DarkGreen);
 
-            _scene.Add(wireframe, new Point3D(0, 0, 0));
+            _scene.Add(_model, new Point3D(0, 0, 0));
+        }
+
+        private void RemoveModelFromView(object _)
+        {
+            _scene.Remove(_model);
+            _model = null;
+            _pageNavigation.GoBack();
         }
 
         private void OnPropertyChanged(string propertyName)
