23965c8bbaa1757a394c681dc40cd8a8d1c9964e
Florian Rager <florianrager90@gmail.com>
2020-07-13 15:26:34 +0200

    Add new project for slic3r api testing

---
+++ src/GCodeViewer.Library/Slic3r.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GCodeViewer.Library
+{
+    public class Slic3r
+    {
+        public string ExecutablePath { get; private set; }
+
+        public Slic3r(string executablePath)
+        {
+            ExecutablePath = executablePath;
+        }
+
+        public string Help()
+        {
+            string strCmdText = ExecutablePath + "--help";
+            System.Diagnostics.Process.Start("CMD.exe", "dir");
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/Playground/Slic3rTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GCodeViewer.Library;
+
+namespace Slic3rTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var slicer = new Slic3r(@"C:\Users\florager\Desktop\slic3r\Slic3r-console");
+            slicer.Help();
+        }
+    }
+}
