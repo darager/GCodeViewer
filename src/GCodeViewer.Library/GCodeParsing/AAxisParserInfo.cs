8d8fe715895a54aee54e3ca151a9038ad93cab06
Florian Rager <florianrager90@gmail.com>
2020-11-13 20:01:55 +0100

    Change borderbrushes of textboxes

--- src/GCodeViewer.Library/GCodeParsing/AAxisParserInfo.cs
+++ src/GCodeViewer.Library/GCodeParsing/AAxisParserInfo.cs
@@ -5,9 +5,9 @@
         public string GCodePattern { get; set; } = "A {{value}}";
 
         public float MinValueAAxis { get; set; } = 0;
-        public float MaxValueAAxis { get; set; } = 0;
+        public float MaxValueAAxis { get; set; } = 90;
 
         public float MinDegreesAAxis { get; set; } = 0;
-        public float MaxDegreesAAxis { get; set; } = 0;
+        public float MaxDegreesAAxis { get; set; } = 90;
     }
 }
