1e5812dc8ab23c02681aeede185bad4c4a2698be
Florian Rager <florianrager90@gmail.com>
2020-11-15 00:40:48 +0100

    Add Validator to the Textboxes

---
+++ src/GCodeViewer.WPF/Settings/GCodePatternValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace GCodeViewer.WPF.Settings
+{
+    public class GCodePatternValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                // TODO: make sure validation works correctly
+                string text = value as string;
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, $"Non valid GCodePattern!");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
